0f6ad75a0f046291063e51349c3ca3a76670ce68
aritropaul <aritro.paul61@gmail.com>
2020-01-28 05:39:14 +0530

    Build v1

---
+++ precache-manifest.37ecbb071ec4e6e6e1fad98c9ade9c94.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b21bea796a42d3852f1bf7d07c360cbe",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5aa5c077e2d67d62a1b9",
+    "url": "/static/css/main.1c917fec.chunk.css"
+  },
+  {
+    "revision": "6b2a6ec64ed4f52e3e8b",
+    "url": "/static/js/2.057041ee.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.057041ee.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5aa5c077e2d67d62a1b9",
+    "url": "/static/js/main.ca739cab.chunk.js"
+  },
+  {
+    "revision": "f47711b29be1b7b340bc",
+    "url": "/static/js/runtime-main.eac56ccc.js"
+  }
+]);
